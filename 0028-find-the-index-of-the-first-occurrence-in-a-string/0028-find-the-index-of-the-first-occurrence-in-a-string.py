7684b5d689cf747764268dc685f17745ecd706f6
rahulptl <33183105+rahulptl@users.noreply.github.com>
2023-10-10 19:12:09 +0530

    Time: 8 ms (92.87%), Space: 13.6 MB (11.69%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -11,9 +11,7 @@ class Solution(object):
 
         lp = 0
         rp = needle_len 
-        print(needle_len, lp, rp)
         while rp<=len(haystack):
-            print(haystack[lp:rp])
             if haystack[lp:rp]==needle:
                 return lp
             else:
