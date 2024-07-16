307973900e9ab8d03eba7d9a3e757e93cfc3315e
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-07-17 01:09:09 +0530

    Time: 42 ms (79.97%), Space: 16.5 MB (35.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -12,39 +12,13 @@ class Solution:
         }
 
         int_value = 0
-        index = 0
-        while index<len(s):
-            print(index, s[index])
-            if s[index] == "I":
-                if (index+1)<(len(s)) and s[index+1] == "V":
-                    int_value += 4
-                    index+=1
-                elif (index+1)<(len(s)) and s[index+1] == 'X':
-                    int_value+=9
-                    index+=1
-                else:
-                    int_value+=mapping.get(s[index]) 
-            elif s[index] == 'X':
-                if (index+1)<(len(s)) and s[index+1] == 'L':
-                    int_value+=40
-                    index+=1
-                elif (index+1)<(len(s)) and s[index+1] == 'C':
-                    int_value+=90
-                    index+=1
-                else:
-                    int_value+=mapping.get(s[index])    
+        prev_val = 0
+        for char in reversed(s):
 
-            elif s[index] == 'C':
-                if (index+1)<(len(s)) and s[index+1] == 'D':
-                    int_value+=400
-                    index+=1
-                elif (index+1)<(len(s)) and s[index+1] == 'M':
-                    int_value+=900
-                    index+=1
-                else:
-                    int_value+=mapping.get(s[index])    
+            current_val = mapping[char]
+            if current_val < prev_val:
+                int_value -= current_val
             else:
-                int_value+=mapping.get(s[index])  
-            index+=1         
-            print(int_value)
+                int_value+=current_val
+            prev_val = current_val
         return int_value
