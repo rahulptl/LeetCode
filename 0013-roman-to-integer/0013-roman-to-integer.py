a4027dd5a6628a203be30adbaf1c4fdb4fb433c0
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-07-17 01:06:12 +0530

    Time: 66 ms (5.64%), Space: 16.9 MB (9.8%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        value = 0
-        mapper = {
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        mapping = {
             "I" : 1,
             "V" : 5,
             "X" : 10,
@@ -14,46 +10,41 @@ class Solution(object):
             "D" : 500,
             "M" : 1000
         }
-        i=0
-        while i<len(s):
-            if i==len(s)-1:
-                value+=mapper.get(s[i])
-            else:
-                print(s[i], s[i+1], mapper.get(s[i]), mapper.get(s[i+1]))
-                if s[i] == 'I':
-                    if s[i+1] == 'V':
-                        value+=(mapper.get(s[i+1]) - mapper.get(s[i]))
-                        i=i+1
-                    elif s[i+1] == 'X':
-                        value+=(mapper.get(s[i+1]) - mapper.get(s[i]))
-                        i=i+1
-                    else:
-                        value+=mapper.get(s[i])
 
-                elif s[i] == 'X':
-                    if s[i+1] == 'L':
-                        value+=(mapper.get(s[i+1]) - mapper.get(s[i]))
-                        i=i+1
-                    elif s[i+1] == 'C':
-                        value+=(mapper.get(s[i+1]) - mapper.get(s[i]))
-                        i=i+1
-                    else:
-                        value+=mapper.get(s[i])
-
-
-                elif s[i] =='C':
-                    if s[i+1] == 'D':
-                        value+=(mapper.get(s[i+1]) - mapper.get(s[i]))
-                        i=i+1
-                    elif s[i+1] == 'M':
-                        value+=(mapper.get(s[i+1]) - mapper.get(s[i]))
-                        i=i+1
-                    else:
-                        value+=mapper.get(s[i])
-                
+        int_value = 0
+        index = 0
+        while index<len(s):
+            print(index, s[index])
+            if s[index] == "I":
+                if (index+1)<(len(s)) and s[index+1] == "V":
+                    int_value += 4
+                    index+=1
+                elif (index+1)<(len(s)) and s[index+1] == 'X':
+                    int_value+=9
+                    index+=1
                 else:
-                    value+=mapper.get(s[i])
-            i=i+1
-            print(value)
-        return value
+                    int_value+=mapping.get(s[index]) 
+            elif s[index] == 'X':
+                if (index+1)<(len(s)) and s[index+1] == 'L':
+                    int_value+=40
+                    index+=1
+                elif (index+1)<(len(s)) and s[index+1] == 'C':
+                    int_value+=90
+                    index+=1
+                else:
+                    int_value+=mapping.get(s[index])    
 
+            elif s[index] == 'C':
+                if (index+1)<(len(s)) and s[index+1] == 'D':
+                    int_value+=400
+                    index+=1
+                elif (index+1)<(len(s)) and s[index+1] == 'M':
+                    int_value+=900
+                    index+=1
+                else:
+                    int_value+=mapping.get(s[index])    
+            else:
+                int_value+=mapping.get(s[index])  
+            index+=1         
+            print(int_value)
+        return int_value
