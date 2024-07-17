9610bd4084d6f0e80ce91d3d29d9699eca1977ce
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-07-18 01:11:09 +0530

    Time: 50 ms (42.02%), Space: 16.5 MB (41.27%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,20 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        mapping = {1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V', 1: 'I'}        
-        def get_closest_value(i):
-            for k,v in mapping.items():
-                if str(i).startswith('4') or str(i).startswith('9'):
-                    main_value = int(str(int(str(i)[0])+1) + '0'*len(str(i)[1:]))
-                    sub_value = int('1'+'0'*len(str(i)[1:]))
-                    return f"{mapping.get(sub_value)}{mapping.get(main_value)}", i-(main_value-sub_value)
-                if i-k>=0:
-                    return v, (i-k)
-            
-        output = ""
-        while num>0:
-            string, num = get_closest_value(num)
-            output+=string
-            #print(output, num)
-        return output
-
+        values = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+            (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
         
+        roman = []
+        for value, symbol in values:
+            while num >= value:
+                roman.append(symbol)
+                num -= value
+        return ''.join(roman)
