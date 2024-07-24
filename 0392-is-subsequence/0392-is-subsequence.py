a03aab061fd8c434e5cfef7a4f5b1382951a06e7
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-07-25 00:02:21 +0530

    Time: 35 ms (63.01%), Space: 16.6 MB (57.74%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,9 +4,9 @@ class Solution:
         ptr = 0
         found = 0
         for char in s:
-            print(f"{char=}, {ptr=}")
+            #print(f"{char=}, {ptr=}")
             while(ptr<len(t) and t[ptr]!=char ):
-                print(f"{t[ptr]=}")
+                #print(f"{t[ptr]=}")
                 ptr+=1
             if(ptr<len(t)):
                 ptr+=1
