2521175ebffce34766c0ce782370e0b89fee39cf
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-07-27 00:24:20 +0530

    Time: 544 ms (13.47%), Space: 29.9 MB (19.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -8,12 +8,11 @@ class Solution:
         while(lp<rp):
 
             max_vol = max(max_vol, (min(height[rp],height[lp]))*(rp-lp))
-            if height[lp] < height[rp]:
+            if height[lp] <= height[rp]:
                 lp+=1
             elif height[lp] > height[rp]:
                 rp-=1
-            else:
-                lp+=1
+
             #print(max_vol, height[lp], height[rp])
 
         return max_vol
