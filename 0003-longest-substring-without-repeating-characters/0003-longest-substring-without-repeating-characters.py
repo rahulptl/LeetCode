0f7fb23b80da0bd5c9cbd16dea8676b2a6605946
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-07-29 23:34:38 +0530

    Time: 71 ms (23%), Space: 16.6 MB (32.69%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        if not s:
+            return 0
+        longest_substr = 0
+
+        sp = 0
+        fp = 0
+        mapping = {}
+        print(len(s))
+        while(fp<len(s)):
+            if s[fp] in mapping.keys():       
+                pointer_to_remove_till =    mapping[s[fp]]      
+                while sp<=pointer_to_remove_till:
+                    #print(f"{sp=}, popping {s[sp]}")
+                    mapping.pop(s[sp])
+                    sp+=1
+            else:
+                mapping[s[fp]] = fp
+                fp+=1
+            longest_substr = max(longest_substr, (fp-sp))
+            #print(f"{sp=} {fp=} {mapping=} {longest_substr}")
+
+
+        return longest_substr
+
