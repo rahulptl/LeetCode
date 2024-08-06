519c83c55dd9a13c2d5611d2979c54885bb464ae
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-08-07 01:20:27 +0530

    Time: 43 ms (16.86%), Space: 16.6 MB (14.84%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -11,40 +11,42 @@ class Solution:
 
             neighbours = 0
             status = 0
+            
+            pairs = [
+                (i-1,j-1),
+                (i-1,j),
+                (i-1,j+1),
+                (i,j+1),
+                (i+1,j+1),
+                (i+1,j),
+                (i+1,j-1),
+                (i,j-1)
+            ]
+            print(pairs)
+            for x,y in pairs:
+                if 0<=x<nrows and 0<=y<ncolumns:
+                    if board[x][y]==2:
+                        val_to_add = 0
+                    elif board[x][y]==-1:
+                        val_to_add = 1
+                    else:
+                        val_to_add = board[x][y]
 
-            if i-1>=0 and j-1>=0:
-                status += board[i-1][j-1]
-                neighbours+=1
-            if i-1>=0:
-                status += board[i-1][j]
-                neighbours+=1
-            if i-1>=0 and j+1<ncolumns:
-                status += board[i-1][j+1]
-                neighbours+=1
-            if j+1<ncolumns:
-                status += board[i][j+1]
-                neighbours+=1
-            if i+1<nrows and j+1<ncolumns:
-                status += board[i+1][j+1]
-                neighbours+=1
-            if i+1<nrows:
-                status += board[i+1][j]
-                neighbours+=1
-            if i+1<nrows and j-1>=0:
-                status += board[i+1][j-1]
-                neighbours+=1
-            if j-1>=0:
-                status += board[i][j-1]
-                neighbours+=1
-            if board[i][j]==0 and status==3:
-                return 1
+                    status+=val_to_add
+
+
+            if board[i][j]==0:
+                if status==3:
+                    return 2
+                else:
+                    return 0
             else:
                 if status<2:
-                    return 0
+                    return -1
                 if status==2 or status==3:
                     return board[i][j]
                 if status>3:
-                    return 0
+                    return -1
         
 
         nrows = len(board)
@@ -54,11 +56,11 @@ class Solution:
         for i in range(nrows):
             for j in range(ncolumns):
                 val = calculate(i,j)
-                new_board[i].append(val)
-            
+                board[i][j] = val
+        
         for i in range(nrows):
             for j in range(ncolumns):
-                board[i][j] = new_board[i][j]
-            
-
-
+                if board[i][j]==-1:
+                    board[i][j]=0
+                if board[i][j]==2:
+                    board[i][j]=1
