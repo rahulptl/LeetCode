c085eb4e5e8cbf499b2b9aa54a5c3bb602c1dae1
rahulptl <33183105+rahulptl@users.noreply.github.com>
2024-08-11 23:04:21 +0530

    Time: 94 ms (39.28%), Space: 20.2 MB (56.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,14 +4,8 @@ class Solution:
 
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        output = {}
+        res = defaultdict(list)
         for i in strs:
-            a = {}
-            for char in i:
-                a[char] = a.get(char,0)+1
-            a = str(dict(sorted(a.items())))
-            if a not in output:
-                output[a] = [i]
-            else:
-                output[a].append(i)
-        return output.values()
+            string = ''.join(sorted(i))  
+            res[string].append(i)    
+        return res.values()
